6e3cb88fc6f8f3a45361e8d0a7de68f9e2c4566d
Sdavis <stevendavis0622@gmail.com>
2019-12-13 15:49:18 -0700

    search fields

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,13 @@ import "./card.css";
 
 export const Card = props => (
   <div>
-    <h1 className="card-container">{props.monster.name}</h1>
+    <h1 className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
+      {props.monster.name}
+      {/* <p>{props.monster.email}</p> */}
+    </h1>
   </div>
 );
